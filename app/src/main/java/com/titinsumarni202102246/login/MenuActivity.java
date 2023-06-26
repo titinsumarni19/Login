310c372d596c8e299ac987839be2255538a7ec8e
titinsumarni19 <92568394+titinsumarni19@users.noreply.github.com>
2023-06-26 10:26:37 +0700

    Initial commit

---
+++ app/src/main/java/com/titinsumarni202102246/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.titinsumarni202102246.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
