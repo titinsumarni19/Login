4dc9f2b757f331a82f3f405f12281ef9e4626222
titinsumarni19 <92568394+titinsumarni19@users.noreply.github.com>
2023-06-28 11:29:10 +0700

    Update MenuActivity.java

--- app/src/main/java/com/titinsumarni202102246/login/MenuActivity.java
+++ app/src/main/java/com/titinsumarni202102246/login/MenuActivity.java
@@ -2,13 +2,28 @@ package com.titinsumarni202102246.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button _viewmahasiswa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        _viewmahasiswa = findViewById(R.id.ViewMahasiswa);
+
+        _viewmahasiswa.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ViewMahasiswa.class);
+                startActivity(intent);
+            }
+        });
     }
 }
