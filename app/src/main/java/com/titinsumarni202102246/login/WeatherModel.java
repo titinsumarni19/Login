c6fd129c66d1d85e9848a4c7f5c3c5666116cc36
titinsumarni19 <92568394+titinsumarni19@users.noreply.github.com>
2023-07-05 06:32:06 +0700

    Create WeatherModel.java

---
+++ app/src/main/java/com/titinsumarni202102246/login/WeatherModel.java
@@ -0,0 +1,34 @@
+package com.titinsumarni202102246.login;
+
+public class WeatherModel {
+    private String main;
+    private String description;
+    private String icon;
+
+    public WeatherModel(){
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
